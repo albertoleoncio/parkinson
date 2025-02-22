08c9ac36329af2beee91c89cacf9219ca68e1e0a
albertoleoncio <albertoleoncio@hotmail.com>
2025-02-22 11:34:57 -0300

    Add new models and migrations

--- data/admin.py
+++ data/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Category, Page, Content
+from .models import Category, Page, Parser, Query, Analysis, Reference, Authorship
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Page)
-admin.site.register(Content)
+admin.site.register(Parser)
+admin.site.register(Query)
+admin.site.register(Analysis)
+admin.site.register(Reference)
+admin.site.register(Authorship)
--- data/models.py
+++ data/models.py
@@ -22,9 +22,50 @@ class Page(models.Model):
     class Meta:
         unique_together = ('referer', 'language_code')
 
-class Content(models.Model):
+class Parser(models.Model):
     page = models.OneToOneField(Page, on_delete=models.CASCADE)
-    content = models.TextField()
+    json = models.JSONField()
+
+    def __str__(self):
+        return self.page.language_code + ' - ' + self.page.referer.article_name
+
+class Query(models.Model):
+    page = models.OneToOneField(Page, on_delete=models.CASCADE)
+    json = models.JSONField()
+
+    def __str__(self):
+        return self.page.language_code + ' - ' + self.page.referer.article_name
+
+class Analysis(models.Model):
+    page = models.OneToOneField(Page, on_delete=models.CASCADE)
+    words = models.IntegerField()
+    images = models.IntegerField()
+    intrawikis = models.IntegerField()
+    sections = models.IntegerField()
+    total_edits = models.IntegerField()
+    first_edit = models.DateTimeField()
+    last_edit = models.DateTimeField()
+    unique_editors = models.IntegerField()
+    
+    def __str__(self):
+        return self.page.language_code + ' - ' + self.page.referer.article_name
+
+class Reference(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    reference = models.TextField()
+    year = models.IntegerField(blank=True)
+    cctld = models.CharField(max_length=10, blank=True)
+    doi = models.CharField(max_length=100, blank=True)
+    pmid = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.page.language_code + ' - ' + self.page.referer.article_name + ' - ' + self.id
+
+class Authorship(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    language_code = models.CharField(max_length=10)
+    author = models.CharField(max_length=200)
+    size = models.IntegerField()
 
     def __str__(self):
         return self.page.language_code + ' - ' + self.page.referer.article_name
